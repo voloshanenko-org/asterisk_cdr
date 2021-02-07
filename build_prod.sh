d8739aeead047c5bd5ce1cd24aa9f28139ca54a6
Igor Voloshanenko <bob@example.com>
2021-02-06 19:04:50 -0500

    Fix stupid typo

--- build_prod.sh
+++ build_prod.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 source env.prod
 docker build -t asterisk_cdr .
-docker tag smsgateway ${DOCKER_REGISTRY}/asterisk_cdr:latest
+docker tag asterisk_cdr ${DOCKER_REGISTRY}/asterisk_cdr:latest
 docker push ${DOCKER_REGISTRY}/asterisk_cdr:latest
 python deploy/deploy.py
